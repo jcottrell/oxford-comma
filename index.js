83a81d9daa9fe723431ddce3e44eb88c174c45a2
Joshua Cottrell <joshua.e.cottrell@gmail.com>
2019-01-25 17:11:39 -0500

    Implements crocks with much less ramda

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const ands = ['chips', 'cheese', '<refreshing beverage>']
 const tricky = ['gravel', '', null, 'stones', [], 'rocks', '']
 const mixed = ['1', 2, 3]
 const objs = [{'one': 1}, {'two': 2}, {'three': 3}]
+const empty = []
 
 const lastly = oc('and lastly ')
 
-console.log(or(ors))      // work, play, or sleep
-console.log(and(ands))    // chips, cheese, and <refreshing beverage>
-console.log(lastly(ors))  // work, play, and lastly sleep
-console.log(and(tricky))  // gravel, stones, and rocks
-console.log(or(mixed))    // 1, 2, or 3
-console.log(and(objs))    // {"one": 1}, {"two": 2}, and {"three": 3}
+console.log('or:          ', or(ors))      // work, play, or sleep
+console.log('and:         ', and(ands))    // chips, cheese, and <refreshing beverage>
+console.log('and lastly:  ', lastly(ors))  // work, play, and lastly sleep
+console.log('tricky and:  ', and(tricky))  // gravel, stones, and rocks
+console.log('mixed or:    ', or(mixed))    // 1, 2, or 3
+console.log('objects and: ', and(objs))    // {"one": 1}, {"two": 2}, and {"three": 3}
+console.log('empty:       ', and(empty))   // ''
--- oxford-comma.js
+++ oxford-comma.js
@@ -1,13 +1,75 @@
-const {and, compose, concat, dropLast, filter, is, isEmpty, isNil, join, last,
-  map, not, toString} = require('ramda')
-const notEmpty = x => and(compose(not, isEmpty)(x), compose(not, isNil)(x))
-const nonStringToString = x => compose(not, is(String))(x) ? toString(x) : x
+const {join, slice, toString} = require('ramda')
 
-const oc = finalWord => compose(
+const and       = require('crocks/logic/and')
+const ifElse    = require('crocks/logic/ifElse')
+const not       = require('crocks/logic/not')
+const or        = require('crocks/logic/or')
+
+const chain     = require('crocks/pointfree/chain')
+const concat    = require('crocks/pointfree/concat')
+const filter    = require('crocks/pointfree/filter')
+const head      = require('crocks/pointfree/head')
+const map       = require('crocks/pointfree/map')
+const option    = require('crocks/pointfree/option')
+
+const compose   = require('crocks/helpers/compose')
+const curry     = require('crocks/helpers/curry')
+
+const identity  = require('crocks/combinators/identity')
+
+const isArray   = require('crocks/predicates/isArray')
+const isEmpty   = require('crocks/predicates/isEmpty')
+const isNil     = require('crocks/predicates/isNil')
+const isNumber  = require('crocks/predicates/isNumber')
+const isString  = require('crocks/predicates/isString')
+
+const safe      = require('crocks/Maybe/safe')
+const {Just, Nothing} = require('crocks/Maybe')
+
+// isNonEmptyArray :: Object -> Boolean
+const isNonEmptyArray = and(not(isEmpty), isArray)
+
+// concatFuns :: Function -> Function -> [a] -> [a]
+const concatFuns = curry((f, g, x) => concat(f(x), g(x)))
+
+// dropLast :: [a] => Maybe [a]
+const dropLast =
+  ifElse(
+    isNonEmptyArray,
+    compose(Just, slice(0, -1)),
+    Nothing)
+
+// last :: [a] -> Maybe a
+const last =
+  ifElse(
+    isNonEmptyArray,
+    compose(head, slice(-1, Infinity)),
+    Nothing)
+
+// notEmpty :: [a] -> Boolean
+const notEmpty = and(
+  or(isNumber, not(isEmpty)),
+  not(isNil))
+
+// nonStringToString :: Object -> String
+const nonStringToString = ifElse(not(isString), toString, identity)
+
+// prependConnectingWord :: String -> [String] -> Maybe [String]
+const prependConnectingWord = curry((connectingWord, wordList) =>
+  chain(
+    compose(Just, x => [x]),
+    concat(
+      last(wordList),
+      safe(isString, connectingWord))))
+
+// oc :: Object -> String
+const oc = connectingWord => compose(
   join(', '),
-  x => concat(dropLast(1, x), [finalWord+last(x)]),
-  map(nonStringToString),
-  filter(notEmpty)
+  option([]),
+  chain(concatFuns(prependConnectingWord(connectingWord), dropLast)),
+  map(map(nonStringToString)),
+  map(filter(notEmpty)),
+  safe(isArray)
 )
 
 module.exports = {
